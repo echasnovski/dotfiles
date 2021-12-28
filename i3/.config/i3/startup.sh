d2873e79f18e4e87d6898a78cef6de45af5e7d62
Evgeni Chasnovski <evgeni.chasnovski@gmail.com>
2021-12-28 21:06:42 +0200

    Update i3 config.
    Details:
    - Simplify startup.
    - Simplify window rules.

--- i3/.config/i3/startup.sh
+++ i3/.config/i3/startup.sh
@@ -1,47 +1,38 @@
 #!/bin/bash
 
-### Code for custom i3 startup actions ###
+#============================================================================#
+#                     Code for custom i3 startup actions                     #
+#============================================================================#
 
+# Enable settings ============================================================
 # Manage keyboard layout
 setxkbmap -layout us,ru
 setxkbmap -option 'grp:alt_shift_toggle'
 
-# Start `compton` to reduce screen tearing
+# Start processes ============================================================
+# `compton` to reduce screen tearing
 compton --config ~/.config/compton.conf -b
 
-# Start process of showing regularly changed wallpapers
+# Process of showing regularly changed wallpapers
 if [[ -f ~/.wallpapers/random-wallpapers.sh ]]
 then
   chmod u+x ~/.wallpapers/random-wallpapers.sh
   ~/.wallpapers/random-wallpapers.sh &
 fi
 
-# Open predefined windows
-## Main terminal
-i3-msg 'workspace "1:main"'
-sleep 0.5
-
-kitty &
-sleep 0.5
-
-## Permanent windows (usually should stay opened)
-## NOTEs:
-## - If command throws error (usually because program is not present), it
-##   should still execute the rest.
-## - Tried to do custom saved layout but decided to go simpler because not all
-##   programs might be present everywhere this setup is run.
-
+# Open predefined windows ====================================================
+# Permanent windows (usually should stay opened)
+# NOTEs:
+# - If command throws error (usually because program is not present), it
+#   should still execute the rest.
+# - Tried to do custom saved layout but decided to go simpler because not all
+#   programs might be present everywhere this setup is run.
 i3-msg 'workspace "10:permanent"; layout splith'
-sleep 0.5
+sleep 1
 
 telegram-desktop &
-sleep 0.5
-
+sleep 1
 thunderbird &
-sleep 0.5
-
-kitty btop &
-sleep 0.5
-
-## Show default "void" workspace with wallpaper
-i3-msg 'workspace "0:void"'
+sleep 1
+kitty --name=permanent-btop btop &
+sleep 1
